d484ca5b048e8b33e17aab964b1a1791ef391c46
Jacquet L <jacquetleme2021@gmail.com>
2023-03-17 19:11:59 -0500

    novidades

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ console.log("Seja bem vindo")
 // como declarar string
 console.log('Jacquet')
 console.log(1 + 1)
+
+console.log({
+    nome: 'James',
+    idade: 20,
+    peso: 80,
+    andar: function(){
+        console.log('Andar')
+    }
+})
+
+//declaration of vaviavél
+var nameF 
+nameF = 'Jacquet'
+//assignment
+
+//que tipo de dado foi colocado na variavél?
+console.log(typeof nameF)
+
+let age, isHuman
+
+age = 20
+isHuman = true
+
+console.log('Age = ' + age +'  Boolean = ' +isHuman)
+
+
